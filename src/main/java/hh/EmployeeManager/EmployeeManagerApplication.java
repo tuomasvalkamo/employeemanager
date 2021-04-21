f6e85c30479e20702fe58ad735902d251d32cdd6
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-04-21 19:44:42 +0300

    Switched DB from H2 to PostGre

--- src/main/java/hh/EmployeeManager/EmployeeManagerApplication.java
+++ src/main/java/hh/EmployeeManager/EmployeeManagerApplication.java
@@ -35,7 +35,11 @@ public class EmployeeManagerApplication {
 			// Create users: admin/admin user/user
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			
+			// Delete all users for Heroku deployment
 			urepository.deleteAll();
+			
+			// Add users to repo
 			urepository.save(user1);
 			urepository.save(user2);
 			
